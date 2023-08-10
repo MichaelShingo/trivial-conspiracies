0599bac686881ab9e1ac6420b0c24618ad053f54
MichaelShingo <mcrawford5376@gmail.com>
2023-08-10 22:32:47 +0200

    flexbox pre-grid

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import eyes from './img/eyes.png';
 import question from './img/question.png';
 import ScrollablePage from './components/ScrollablePage/ScrollablePage';
 import InterviewText from './components/interviewText/InterviewText';
+import creditsContent from './components/ScrollablePage/creditsContent';
 
 function App() {
   const [imagesVisible, setImagesVisible] = useState(true);
@@ -48,111 +49,15 @@ function App() {
 
   return (
     <div className="App">
-      <div id="question-container">
+      {/* <div id="question-container">
             <img src={question}/>
-          </div>
+        </div> */}
       <ScrollablePage
         pageVisible={creditsVisible}
         setPageVisible={setCreditsVisible}
         setImagesVisible={setImagesVisible}
       >
-        <p>It was summertime in the year 2020. We had all been living isolated, in the
-                deep, dead heat of the covid-19 pandemic. I was beginning to lose faith in the
-                prospects of filmmaking amidst this new covid-world. Then one day my friend
-                Shae Belenski sent me a draft of his writing in a document titled: “drunk.” It was
-                essentially a series of monologues, which Shae had written as “interviews” (read
-                these by clicking on the “Trivial Conspiracies” button.) He was interested in
-                filming some of these monologues and I jumped at the opportunity to adapt
-                his writing into a film...
-            </p>
-            <p>For me, making this film was a creative escape from the scary reality of our
-                world, but also a way of processing this reality. Conspiracy theories revolving
-                around the pandemic and the 2020 presidential election were part of the
-                zeitgeist of these years and this project brought some healing levity to all that
-                harmful nonsense.
-            </p>
-
-            <p>I write this exactly 2 years after Philadelphia locked down. I am stuck at home,
-                sick with Covid-19. During my quarantine, I can’t help but reflect on the past
-                two years and feel lucky to be alive, vaccinated, and grateful for the privilege to
-                be creative with people amidst all the chaos.
-            </p>
-            <p>- Sam</p>
-            <ul>
-                <li>Cast (in order of appearance):</li>
-                <li>Brooke Shilling</li>
-                <li>Sam Dellert</li>
-                <li>Nina Sharp</li>
-                <li>Nazlah Black</li>
-                <li>Rohaan Unvala</li>
-                <li>Renee Comings</li>
-                <li>Ryan Pater</li>
-                <li>Xan Pemsler</li>
-                <li>Sasha Kostyrko</li>
-                <li>Anna Phillips</li>
-            </ul>
-            <ul>
-                <li>Director, Cinematographer, Editor</li>
-                <li>Sam Dellert</li>
-            </ul>
-            <ul>
-                <li>Writer</li>
-                <li>Shae Belenski</li>
-            </ul>
-            <ul>
-                <li>Colorist</li>
-                <li>Jake White</li>
-            </ul>
-            <ul>
-                <li>Sound Editor</li>
-                <li>Steve Campagna</li>
-            </ul>
-            <ul>
-                <li>Music from</li>
-                <li>Barraco Barner</li>
-                <li>Jeff Silverman</li>
-            </ul>
-            <ul>
-                <li>Location Sound Mixer</li>
-                <li>Matt Ober</li>
-            </ul>
-            <ul>
-                <li>Boom Operator</li>
-                <li>Xan Pemsler</li>
-            </ul>
-            <ul>
-                <li>Production Support/Set Photographer</li>
-                <li>Renee Comings</li>
-            </ul>
-            <ul>
-                <li>Web Developer</li>
-                <li>Michael Shingo Crawford</li>
-            </ul>
-            <ul>
-                <li>Poster Design by</li>
-                <li>Tawan Maneetapho</li>
-            </ul>
-            <ul>
-                <li>Thank you</li>
-                <li>Martha (Kensington)</li>
-                <li>Liberty Choice</li>
-                <li>Pro 8mm</li>
-                <li>Dana Mandi</li>
-            </ul>
-            <ul>
-                <li>Thank you</li>
-                <li>Dellert Family</li>
-                <li>Julianna Casella</li>
-                <li>Cassidy Minarik</li>
-                <li>Peter Charney</li>
-                <li>Amitanshu Das</li>
-                <li>Megan Wetham</li>
-                <li>Trey Shields</li>
-            </ul>
-            <ul>
-                <li>Proudly filmed in Philadelphia, PA</li>
-                <li>© 2023</li>
-            </ul>
+        {creditsContent}
       </ScrollablePage>
       <ScrollablePage
         pageVisible={interviewVisible}
@@ -172,16 +77,11 @@ function App() {
           videoVisible={videoVisible}
         />
         <div 
-          id="title-container" 
+          id="row-1" 
           className={imagesVisible ? 'flex-row animate-in' : 'flex-row animate-out'}
         >
           
-          {/* <ImageButton 
-            id="question"
-            source={question}
-            handleImageClick={handleQuestionClick}
-            className="question-mark"
-        /> */}
+          
           <ImageButton 
             id="freetime"
             source={freeTime}
@@ -202,7 +102,12 @@ function App() {
           />
         </div>
         <div className={imagesVisible ? 'flex-row animate-in' : 'flex-row animate-out'} id="row-2">
-            
+            <ImageButton 
+                id="question"
+                source={question}
+                handleImageClick={handleQuestionClick}
+                className="question-mark"
+            />
             <ImageButton
               id="pizza"
               source={pizza}
